1d42899f286da9e1a709bf383b78a2340901434c
Shashi <shashikant.py@gmail.com>
2022-02-12 19:41:00 +0530

    gui

---
+++ img.py
@@ -0,0 +1,44 @@
+from tkinter import*
+
+from PIL import Image, ImageTk  #this is used for jpg image 
+
+
+sk_indian_jharokha = Tk()
+sk_indian_jharokha.geometry("500x400")
+sk_indian_jharokha.title("MY YOUTUBE CHANNEL INDIAN JHAROKHA")
+
+# photo = PhotoImage(file="E:\\vid editing\\filmora edit videos\\1.png")
+
+# image = Image.open("C:\\Users\\shashikant\\Desktop\\img2.jpg")
+# photo = ImageTk.PhotoImage(image)z
+    
+# shashi_lable = Label(image=photo) 
+# shashi_lable.pack()
+
+# title_lable = Label(text = "Hi, I am Shashi kant kumar from Bihar. I am purshing B.Tech in CSE from RITS",bg="blue",fg="red", pady="10", padx="20", font=("comicsansms", 19,"bold"), borderwidth="12", relief="solid")
+# title_lable.pack(side= "left", fill=Y, padx="34", pady="34")
+
+
+title_sk= Label(text="REDAY",bg="red")
+title_sk.pack(side="bottom", padx = "44", pady="44")
+
+
+sk_indian_jharokha.mainloop()
+
+
+
+#Lebel tags
+# Bg = backgroung
+# fg=  for ground
+# text = add text 
+# pad = x and 
+# _Relief = border styling - SUNKEN, RAISED,GROOVE,RIDGE
+
+
+
+# pack tags
+# ANCHOR = ne, sw,nw...
+# side = left, BOTTOM, top, RIGHT
+# fill .....x and y
+# padx
+# pady
---
+++ np.py
@@ -0,0 +1,19 @@
+from tkinter import*
+
+indian_jharokha = Tk()
+
+# #  Width x Height 
+# indian_jharokha.geometry("650x475")
+
+# indian_jharokha.minsize(200,100)
+# indian_jharokha.maxsize(1200,1000)
+# yt = Label(text="plase subscribe my youtube channel indian jharokha")
+# yt.pack()
+# indian_jharokha.mainloop()
+
+
+pycharm = Tk()
+pycharm.geometry("730x434")
+sk = Label(text="PyCharm Community Edition")
+sk.pack()
+pycharm.mainloop()
